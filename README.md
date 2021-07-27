ec00f03f2cd993de6235ff2aa11b08e299e8105b
Venkata Praveen Madireddy <32617751+mvenkatapraveen@users.noreply.github.com>
2021-07-27 12:27:45 +0530

    Changes done for files exists

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # MIDC
-MIDC Txn Mapping in AppDynamics
+MIDC(Data Collectors) and Txn Mapping in AppDynamics
 
 ## Description
-This application will generate an excel for Txn configured for MIDC.
+This application is used for generating an excel with Txn configurations for MIDC.
 
 ## Prerequisites
 Node, Javascript
 
 Need to provide two input files:
-midc.json(for specific application)
-txn.json(select specific midc and click on configure txn to get txn json).
-We can get these files from debugger mode.
+<appName> + DC.json(for specific application)
+<appName> + Txn.json(select specific midc and click on configure txn to get txn json).
+These files can be obtained by doing the following:
+1. Login to AppDynamics
+2. Select Application  --> Configuration --> Instrumentation --> DataCollectors
+3. Open Developer Tools in browser
+4. Refresh the page
+5. Copy the data collectors from response for specific request and create a DC json file 
+6. Now select specific DC and click on Configure Txns for this DC
+7. Copy the txn mappings from response for specific request and create a Txn json file 
+
 
 To start the server
 ```sh
 node server.js
 ```
-This will generate an xls file in output folder.
+
+Load the following in the browser
+http://localhost:3000/midc/<appName>
+
+This will generate an xls file with <appName>+MIDC.xls and Txn mappings in output folder.
--- server.js
+++ server.js
@@ -10,93 +10,114 @@ app.listen(process.env.PORT || 3000, () => {
     console.log('Server listening on port');
 });
 
-app.get('/midc', (req,res)=> {
-    const midcData = fs.readFileSync('./mappings/CrmgwDC.json', 
-    {encoding:'utf8', flag:'r'});
-    const parsedMidcData =  JSON.parse(midcData);
-    let content = "";
-    let header="DataGathererID"+"\t"+"Name"+"\t"+" attachToNewDiscoveredBTs"+"\t"+"enabledForApm"+"\t"+"enabledForAnalytics"+"\t"+"className"+"\t"+"methodName"+"\t"+"matchCondition"+"\t"+"midc"+"\t"+"txnNames"+"\n";
-    content = content + header;
-    for (const element of parsedMidcData) {
-        if("pojo" == element["type"]) {
-            const id = element["id"];
-            const name = element["name"];
-            const attachToNewDiscoveredBTs = element["attachToNewDiscoveredBTs"];
-            const enabledForApm = element["enabledForApm"];
-            const enabledForAnalytics = element["enabledForAnalytics"];
-            const className =element["definition"]["className"];
-            const methodName = element["definition"]["methodName"];
-
-            const methodParameterTypes = element["definition"]["methodParameterTypes"];
-            const matchConditions = element["definition"]["matchConditions"];
-            let idx = 0;
-            let matchConditionString = "";
-            for (const matchConditionElements of matchConditions) {
-                let value = midcValue(matchConditionElements["methodDataGathererConfig"]["gathererType"], matchConditionElements["methodDataGathererConfig"]["position"], matchConditionElements["methodDataGathererConfig"]["objectDataTransformer"]["transformerType"], matchConditionElements["methodDataGathererConfig"]["objectDataTransformer"]["objectStateGetterMethods"]);
-                value = value + " " +matchConditionElements["match"]["matchType"] + " " + matchConditionElements["match"]["matchPattern"]
-    
-                if(idx>0) {
-                    matchConditionString = matchConditionString+";"+ value;
-                }
-                else {
-                    matchConditionString = matchConditionString+ value;
-                }
-                idx++;
-                
-                //console.log(value);
-            }
-            //console.log("****\n"+matchConditionString)
-            const methodDataGathererConfigs = element["methodDataGathererConfigs"];
-            let midcString = ""
-            idx =0;
-            for (const midcElement of methodDataGathererConfigs) {
-                const midcName = midcElement["name"];
-                let value = midcValue(midcElement["gathererType"], midcElement["position"], midcElement["objectDataTransformer"]["transformerType"],midcElement["objectDataTransformer"]["objectStateGetterMethods"]);
-                
-                if(idx > 0) {
-                    midcString = midcString+";"+ midcName+ " :: "+ value;
-                }  
-                else {
-                    midcString = midcString+ midcName+ " :: "+ value;
-                }
-                idx++;
-            }
-
-            const txnData = fs.readFileSync('./mappings/CrmgwTxn.json', 
+app.get('/midc/:application', (req,res)=> {
+    const appName = req.params.application;
+    const dcFilePath = './mappings/'+appName+'DC.json';
+    const txnFilePath = './mappings/'+appName+'Txn.json';
+    const dcFileExists = fs.existsSync(dcFilePath);
+    const txnFileExists = fs.existsSync(txnFilePath);
+    if(dcFileExists && txnFileExists) {
+        const midcData = fs.readFileSync(dcFilePath, 
             {encoding:'utf8', flag:'r'});
-            const parsedTxnData =  JSON.parse(txnData);
-            let txnNames="";
+            const parsedMidcData =  JSON.parse(midcData);
+            let content = "";
+            let header="DataGathererID"+"\t"+"Name"+"\t"+" attachToNewDiscoveredBTs"+"\t"+"enabledForApm"+"\t"+"enabledForAnalytics"+"\t"+"className"+"\t"+"methodName"+"\t"+"matchCondition"+"\t"+"midc"+"\t"+"txnNames"+"\n";
+            content = content + header;
+            for (const element of parsedMidcData) {
+                if("pojo" == element["type"]) {
+                    const id = element["id"];
+                    const name = element["name"];
+                    const attachToNewDiscoveredBTs = element["attachToNewDiscoveredBTs"];
+                    const enabledForApm = element["enabledForApm"];
+                    const enabledForAnalytics = element["enabledForAnalytics"];
+                    const className =element["definition"]["className"];
+                    const methodName = element["definition"]["methodName"];
+        
+                    const methodParameterTypes = element["definition"]["methodParameterTypes"];
+                    const matchConditions = element["definition"]["matchConditions"];
+                    let idx = 0;
+                    let matchConditionString = "";
+                    for (const matchConditionElements of matchConditions) {
+                        let value = midcValue(matchConditionElements["methodDataGathererConfig"]["gathererType"], matchConditionElements["methodDataGathererConfig"]["position"], matchConditionElements["methodDataGathererConfig"]["objectDataTransformer"]["transformerType"], matchConditionElements["methodDataGathererConfig"]["objectDataTransformer"]["objectStateGetterMethods"]);
+                        value = value + " " +matchConditionElements["match"]["matchType"] + " " + matchConditionElements["match"]["matchPattern"]
             
-            for(let txnElement of parsedTxnData) {
-                
-                let dataConfigIds = txnElement["dataGathererConfigIds"];
-                for(let configId of dataConfigIds) {
-                    if (id == configId) {
-                        if("" == txnNames) {
-                            txnNames = txnNames +txnElement["internalName"];
+                        if(idx>0) {
+                            matchConditionString = matchConditionString+";"+ value;
                         }
                         else {
-                            txnNames = txnNames + ";"+txnElement["internalName"];
+                            matchConditionString = matchConditionString+ value;
                         }
-                        break;
+                        idx++;
+                        
+                        //console.log(value);
                     }
+                    //console.log("****\n"+matchConditionString)
+                    const methodDataGathererConfigs = element["methodDataGathererConfigs"];
+                    let midcString = ""
+                    idx =0;
+                    for (const midcElement of methodDataGathererConfigs) {
+                        const midcName = midcElement["name"];
+                        let value = midcValue(midcElement["gathererType"], midcElement["position"], midcElement["objectDataTransformer"]["transformerType"],midcElement["objectDataTransformer"]["objectStateGetterMethods"]);
+                        
+                        if(idx > 0) {
+                            midcString = midcString+";"+ midcName+ " :: "+ value;
+                        }  
+                        else {
+                            midcString = midcString+ midcName+ " :: "+ value;
+                        }
+                        idx++;
+                    }
+        
+                    const txnData = fs.readFileSync(txnFilePath, 
+                    {encoding:'utf8', flag:'r'});
+                    const parsedTxnData =  JSON.parse(txnData);
+                    let txnNames="";
                     
+                    for(let txnElement of parsedTxnData) {
+                        
+                        let dataConfigIds = txnElement["dataGathererConfigIds"];
+                        for(let configId of dataConfigIds) {
+                            if (id == configId) {
+                                if("" == txnNames) {
+                                    txnNames = txnNames +txnElement["internalName"];
+                                }
+                                else {
+                                    txnNames = txnNames + ";"+txnElement["internalName"];
+                                }
+                                break;
+                            }
+                            
+                        }
+                        
+                    }
+        
+                    let row = id + "\t" +name + "\t" + attachToNewDiscoveredBTs+ "\t" + enabledForApm+ "\t" + enabledForAnalytics+ "\t" + className+ "\t" + methodName+ "\t" + matchConditionString+ "\t" + midcString +  "\t" + txnNames +"\n";
+                    content = content + row;
                 }
                 
+                
+                
             }
-
-            let row = id + "\t" +name + "\t" + attachToNewDiscoveredBTs+ "\t" + enabledForApm+ "\t" + enabledForAnalytics+ "\t" + className+ "\t" + methodName+ "\t" + matchConditionString+ "\t" + midcString +  "\t" + txnNames +"\n";
-            content = content + row;
+            fs.appendFile('./output/'+appName+'MIDC.xls', content, (err) => {
+                if (err) throw err;
+                console.log('File created');
+             });
+            res.send(content);
+    }
+    else {
+        if(!dcFileExists && !txnFileExists) {
+            res.send(dcFilePath + " and "+ txnFilePath + " Not Found");
+        }
+        else if(!dcFileExists) {
+            res.send(dcFilePath + " Not Found");
+        }
+        else if(!txnFileExists) {
+            res.send(txnFilePath + " Not Found");
         }
         
-        
-        
     }
-    fs.appendFile('./output/CrmgwMIDC.xls', content, (err) => {
-        if (err) throw err;
-        console.log('File created');
-     });
-    res.send(content);
+
+    
 });
 
 
